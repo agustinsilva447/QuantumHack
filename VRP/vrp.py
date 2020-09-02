e284e0bbe573c9daeac760f55a2fa1eeb6fa0c07
agustinsilva447 <agustinsilva447@gmail.com>
2020-09-01 21:15:31 -0300

    vrp

--- VRP/vrp.py
+++ VRP/vrp.py
@@ -1,14 +1,10 @@
-# [START program]
-"""Simple Vehicles Routing Problem."""
-
-# [START import]
-from __future__ import print_function
+"""Vehicle Routing Problem"""
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
-# [END import]
 
-
-# [START data_model]
+###########################
+# Problem Data Definition #
+###########################
 def create_data_model():
     """Stores the data for the problem."""
     data = {}
@@ -34,13 +30,13 @@ def create_data_model():
     data['num_vehicles'] = 4
     data['depot'] = 0
     return data
-    # [END data_model]
 
-
-# [START solution_printer]
-def print_solution(data, manager, routing, solution):
-    """Prints solution on console."""
-    print('Objective: {}'.format(solution.ObjectiveValue()))
+###########
+# Printer #
+###########
+def print_solution(data, manager, routing, assignment):
+    """Prints assignment on console."""
+    print('Objective: {}'.format(assignment.ObjectiveValue()))
     total_distance = 0
     for vehicle_id in range(data['num_vehicles']):
         index = routing.Start(vehicle_id)
@@ -49,7 +45,7 @@ def print_solution(data, manager, routing, solution):
         while not routing.IsEnd(index):
             plan_output += ' {} ->'.format(manager.IndexToNode(index))
             previous_index = index
-            index = solution.Value(routing.NextVar(index))
+            index = assignment.Value(routing.NextVar(index))
             route_distance += routing.GetArcCostForVehicle(
                 previous_index, index, vehicle_id)
         plan_output += ' {}\n'.format(manager.IndexToNode(index))
@@ -57,30 +53,23 @@ def print_solution(data, manager, routing, solution):
         print(plan_output)
         total_distance += route_distance
     print('Total Distance of all routes: {}m'.format(total_distance))
-    # [END solution_printer]
 
-
+########
+# Main #
+########
 def main():
-    """Entry point of the program."""
+    """Entry point of the program"""
     # Instantiate the data problem.
-    # [START data]
     data = create_data_model()
-    # [END data]
 
     # Create the routing index manager.
-    # [START index_manager]
     manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
                                            data['num_vehicles'], data['depot'])
-    # [END index_manager]
 
     # Create Routing Model.
-    # [START routing_model]
     routing = pywrapcp.RoutingModel(manager)
 
-    # [END routing_model]
-
     # Create and register a transit callback.
-    # [START transit_callback]
     def distance_callback(from_index, to_index):
         """Returns the distance between the two nodes."""
         # Convert from routing variable Index to distance matrix NodeIndex.
@@ -89,32 +78,32 @@ def main():
         return data['distance_matrix'][from_node][to_node]
 
     transit_callback_index = routing.RegisterTransitCallback(distance_callback)
-    # [END transit_callback]
 
     # Define cost of each arc.
-    # [START arc_cost]
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
-    # [END arc_cost]
+
+    # Add Distance constraint.
+    dimension_name = 'Distance'
+    routing.AddDimension(
+        transit_callback_index,
+        0,  # no slack
+        3000,  # vehicle maximum travel distance
+        True,  # start cumul to zero
+        dimension_name)
+    distance_dimension = routing.GetDimensionOrDie(dimension_name)
+    distance_dimension.SetGlobalSpanCostCoefficient(100)
 
     # Setting first solution heuristic.
-    # [START parameters]
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
     search_parameters.first_solution_strategy = (
         routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-    # [END parameters]
 
     # Solve the problem.
-    # [START solve]
-    solution = routing.SolveWithParameters(search_parameters)
-    # [END solve]
+    assignment = routing.SolveWithParameters(search_parameters)
 
     # Print solution on console.
-    # [START print_solution]
-    if solution:
-        print_solution(data, manager, routing, solution)
-    # [END print_solution]
-
+    if assignment:
+        print_solution(data, manager, routing, assignment)
 
 if __name__ == '__main__':
-    main()
-    # [END program]
+  main()
